c7f497c9fbe5f8ca630f62995d5a9a5da7f74f17
Stewart125 <stewart.whitehead@connect.qut.edu.au>
2018-02-07 17:22:40 +1000

    Initial commit

---
+++ name-sorter/NameSorter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace name_sorter
+{
+    public class NameSorter
+    {
+
+        static void Main(string[] args)
+        {
+            // Read the names from the text file and store in local variable.
+            string[] names = File.ReadAllLines(args[0]);
+
+            // Call the QuickSort() function on the names array from beginning to end.
+            QuickSort(names, 0, names.Length - 1);
+
+            PrintToConsole(names);
+
+            // Save the new sorted array of names to an external text file.
+            System.IO.File.WriteAllLines("./sorted-names-list.txt", names);
+
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// Quicksort Algorithm which sorts an array of strings into alphabetical order.
+        /// </summary>
+        /// <param name="a">the pivot point or middle value of the segment</param>
+        /// <param name="start">the first element of the segment</param>
+        /// <param name="end">the last element in the segment</param>
+        public static void QuickSort(string[] a, int start, int end)
+        {
+            int i = start;
+            int j = end;
+            string pivotPoint = a[(start + end) / 2];
+
+            // While the starting value is less than the end value
+            while (i <= j)
+            {
+                while (a[i].CompareTo(pivotPoint) < 0)
+                {
+                    i++;
+                }
+
+                while (a[j].CompareTo(pivotPoint) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap the two values
+                    string temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls for divided segments
+            if (start < j)
+            {
+                QuickSort(a, start, j);
+            }
+
+            if (i < end)
+            {
+                QuickSort(a, i, end);
+            }
+        }
+
+
+        /// <summary>
+        /// Prints a string array to the console.
+        /// </summary>
+        /// <param name="nameArray">A string array containing the names to be printed</param>
+        public static void PrintToConsole(string[] nameArray)
+        {
+            foreach (string name in nameArray)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+    }
+}
---
+++ name-sorterTests/NameSorterTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using name_sorter;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name_sorter.Tests
+{
+    [TestClass()]
+    public class NameSorterTests
+    {
+        // Standard alphabet character quicksort test
+        [TestMethod()]
+        public void AlphabetCharacterQuickSortTest()
+        {
+            // Arrange parameters
+            string[] unsortedAlphabetArray = { "dddd", "cccc", "bbbb", "aaaa" };
+            string[] sortedAlphabetArray = { "aaaa", "bbbb", "cccc", "dddd" };
+
+            // QuickSort
+            NameSorter.QuickSort(unsortedAlphabetArray, 0, unsortedAlphabetArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(unsortedAlphabetArray, sortedAlphabetArray);
+        }
+
+        // Catch incorrectly sorted list
+        [TestMethod()]
+        public void IncorrectlySortedQuickSortTest()
+        {
+            // Arrange parameters
+            string[] unsortedAlphabetArray = { "dddd", "cccc", "bbbb", "aaaa" };
+            string[] incorrectlySortedAlphabetArray = { "aaaa", "bbbb", "dddd", "cccc" };
+
+            // QuickSort
+            NameSorter.QuickSort(unsortedAlphabetArray, 0, unsortedAlphabetArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreNotEqual(unsortedAlphabetArray, incorrectlySortedAlphabetArray);
+        }
+
+        // Duplicate entries quicksort test
+        [TestMethod()]
+        public void DuplicateCaseAlphabetCharacterQuickSortTest()
+        {
+            // Arrange parameters
+            string[] unsortedAlphabetArray = { "aaaa", "bbbb", "bbbb", "aaaa" };
+            string[] sortedAlphabetArray = { "aaaa", "aaaa", "bbbb", "bbbb" };
+
+            // QuickSort
+            NameSorter.QuickSort(unsortedAlphabetArray, 0, unsortedAlphabetArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(unsortedAlphabetArray, sortedAlphabetArray);
+        }
+
+        // Numeric character test
+        [TestMethod()]
+        public void NumericCharacterQuickSortTest()
+        {
+            // Arrange parameters
+            string[] unsortedAlphabetArray = { "3333", "1111", "2222", "0000" };
+            string[] sortedAlphabetArray = { "0000", "1111", "2222", "3333" };
+
+            // QuickSort
+            NameSorter.QuickSort(unsortedAlphabetArray, 0, unsortedAlphabetArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(unsortedAlphabetArray, sortedAlphabetArray);
+        }
+
+        // Alphanumeric characters quicksort test
+        [TestMethod()]
+        public void AlphanumericCharactersQuickSortTest()
+        {
+            // Arrange parameters
+            string[] unsortedAlphabetArray = { "a1c3", "1337", "33d3", "9cd3" };
+            string[] sortedAlphabetArray = { "1337", "33d3", "9cd3", "a1c3" };
+
+            // QuickSort
+            NameSorter.QuickSort(unsortedAlphabetArray, 0, unsortedAlphabetArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(unsortedAlphabetArray, sortedAlphabetArray);
+        }
+
+        // Strings including spaces quicksort test
+        [TestMethod()]
+        public void StringsWithSpacesQuickSortTest()
+        {
+            // Arrange parameters
+            string[] unsortedAlphabetArray = { "aaaa bbbb", "bbbb aaaa", "aaaa aaaa", "bbbb bbbb" };
+            string[] sortedAlphabetArray = { "aaaa aaaa", "aaaa bbbb", "bbbb aaaa", "bbbb bbbb" };
+
+            // QuickSort
+            NameSorter.QuickSort(unsortedAlphabetArray, 0, unsortedAlphabetArray.Length - 1);
+
+            // Assert
+            CollectionAssert.AreEqual(unsortedAlphabetArray, sortedAlphabetArray);
+        }
+    }
+}
